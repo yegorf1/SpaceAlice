9a2c7514a725ba991dd20574e50ddd1ba449617d
yegorf1 <yegorf1@gmail.com>
2018-12-27 20:32:16 +0300

    Add simple state machine class

---
+++ src/SpaceAlice.Core/States/StateMachine.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SpaceAlice.Core.Exceptions;
+using SpaceAlice.Core.States;
+
+namespace SpaceAlice.Core {
+    public class StateMachine {
+        private readonly Dictionary<string, IState> States;
+        
+        public StateMachine() {
+            States = new Dictionary<string, IState>();
+            
+            LookForStates();
+        }
+
+        private void LookForStates() {
+            AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => typeof(IState).IsAssignableFrom(type))
+                .ToList()
+                .ForEach(RegisterStateType);
+        }
+
+        public void RegisterStateType(Type type) {
+            var instance = (IState) Activator.CreateInstance(type);
+            RegisterStateInstance(instance);
+        }
+
+        public void RegisterStateInstance(IState instance) {
+            if (ContainsState(instance.Name)) {
+                throw new StateMachineException($"State with name '{instance.Name}' already exists.");
+            }
+
+            States.Add(instance.Name, instance);
+        }
+
+        private bool ContainsState(string instanceName) => States.ContainsKey(instanceName);
+    }
+}
